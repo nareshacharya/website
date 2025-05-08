57f35111a9ea053ee9cebcf255e4e7cea1b40caa
nareshacharya <pentapati.naresh@gmail.com>
2025-05-08 15:39:55 +0530

    Added responsive sticky navbar with hover and burger menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,33 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import { useState } from 'react';
+import './Navbar.css';
 
-const Navbar = () => (
-  <nav>
-    <ul>
-      <li><Link to="/">Home</Link></li>
-      <li><Link to="/about">About</Link></li>
-      <li><Link to="/portfolio">Portfolio</Link></li>
-      <li><Link to="/blogs">Blogs</Link></li>
-      <li><Link to="/contact">Contact</Link></li>
-    </ul>
-  </nav>
-);
+const Navbar = () => {
+  const location = useLocation();
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => setMenuOpen(!menuOpen);
+  const closeMenu = () => setMenuOpen(false);
+
+  return (
+    <nav className="navbar">
+      <div className="logo">Naresh</div>
+
+      <div className={`nav-links ${menuOpen ? 'open' : ''}`}>
+        <Link to="/" onClick={closeMenu} className={location.pathname === '/' ? 'active' : ''}>Home</Link>
+        <Link to="/about" onClick={closeMenu} className={location.pathname === '/about' ? 'active' : ''}>About</Link>
+        <Link to="/portfolio" onClick={closeMenu} className={location.pathname === '/portfolio' ? 'active' : ''}>Portfolio</Link>
+        <Link to="/blogs" onClick={closeMenu} className={location.pathname === '/blogs' ? 'active' : ''}>Blogs</Link>
+        <Link to="/contact" onClick={closeMenu} className={location.pathname === '/contact' ? 'active' : ''}>Contact</Link>
+      </div>
+
+      <div className="burger" onClick={toggleMenu}>
+        <div className={`bar ${menuOpen ? 'open' : ''}`}></div>
+        <div className={`bar ${menuOpen ? 'open' : ''}`}></div>
+        <div className={`bar ${menuOpen ? 'open' : ''}`}></div>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
